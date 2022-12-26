ed061b25bcf693c5db0fed2b0223fba32240a536
umuea <elven-rumba0c@icloud.com>
2022-12-26 22:39:40 +0900

    배포 변경 사항

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,5 +1,1 @@
 from .base import *
-
-DEBUG = False
-ALLOWED_HOST = ["ootag.pythonanywhere.com"]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,1 +1,5 @@
 from .base import *
+
+DEBUG = False
+ALLOWED_HOST = ["ootag.pythonanywhere.com"]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
